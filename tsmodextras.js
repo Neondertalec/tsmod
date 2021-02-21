0261ab5a16446f4856ad779d4ac04ff41ab47224
NeonDep <39996170+Neondertalec@users.noreply.github.com>
2021-02-21 21:46:30 +0200

    Update tsmodextras.js

--- tsmodextras.js
+++ tsmodextras.js
@@ -1,3 +1,8 @@
+// ==UserScript==
+// @version     1.1.0
+// ==/UserScript==
 window.tags = {
-	'[SCR]':['DepressionOwU']
+	'[SCR]':['DepressionOwU'],
+	'[TS]': ['ylzaac😎', 'Creazy', 'Priox', 'Aries', 'Goldy', 'drippyk', 'SANDWICH', 'Damasus', '☺♣○•♣♥☻♦♠◘'],
+	'[TO]': ['Darklight', 'Jayyyyyyyyyyyyyy', 'asdfasdfasdf1234', 'Pasemrus', 'thiccsucc']
 }
