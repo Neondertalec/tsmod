0bd4a544a1814230b02ccf1c3be2ea9a753bddde
NeonDep <39996170+Neondertalec@users.noreply.github.com>
2021-02-22 14:47:04 +0200

    Delete tsmodextras.js

--- tsmodextras.js
+++
@@ -1,8 +0,0 @@
-// ==UserScript==
-// @version     1.2.0
-// ==/UserScript==
-window.tags = {
-	'[SCR]':['DepressionOwU'],
-	'[TS]': ['ylzaac😎', 'Creazy', 'Priox', 'Aries', 'Goldy', 'drippyk', 'SANDWICH', 'Damasus', '☺♣○•♣♥☻♦♠◘'],
-	'[TO]': ['Darklight', 'Jayyyyyyyyyyyyyy', 'asdfasdfasdf1234', 'Pasemrus', 'thiccsucc']
-}
