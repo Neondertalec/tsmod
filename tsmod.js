65f48c6c4ea4962ef7f180012f8437a7779ccd66
NeonDep <39996170+Neondertalec@users.noreply.github.com>
2021-03-23 21:51:28 +0200

    Update tsmod.js

--- tsmod.js
+++ tsmod.js
@@ -1,6 +1,6 @@
 // ==UserScript== 
 // @name        TS-Mod
-// @version     1.1.18
+// @version     1.1.19
 // @description	Evades.io TS script.
 // @author      Script by: MeOw:3 (🎀Depression🎀#5556), Most ideas: Piger (Piger#2917).
 // @match       https://evades.io/*
